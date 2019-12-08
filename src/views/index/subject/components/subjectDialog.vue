d8c6c699eeda1ab7fa142b7882f3ba26dbbea7ef
autumnFish <517729329@qq.com>
2019-12-08 21:04:03 +0800

    学科模块 -数据新增

--- src/views/index/subject/components/subjectDialog.vue
+++ src/views/index/subject/components/subjectDialog.vue
@@ -4,45 +4,76 @@
     class="subject-dialog"
     :visible.sync="$parent.addFormVisible"
   >
-    <el-form :model="form">
-      <el-form-item label="学科编号" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+    <el-form :model="addForm" ref="addForm" :rules="rules">
+      <el-form-item label="学科编号" prop="rid" :label-width="formLabelWidth">
+        <el-input v-model="addForm.rid" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="学科名称" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+      <el-form-item label="学科名称" prop="name" :label-width="formLabelWidth">
+        <el-input v-model="addForm.name" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="学科简称" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+        <el-input v-model="addForm.short_name" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="学科简介" :label-width="formLabelWidth">
         <el-input
           type="textarea"
           rows="3"
-          v-model="form.name"
+          v-model="addForm.intro"
           autocomplete="off"
         ></el-input>
       </el-form-item>
       <el-form-item label="学科备注" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"></el-input>
+        <el-input v-model="addForm.remark" autocomplete="off"></el-input>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button @click="$parent.addFormVisible = false">取 消</el-button>
-      <el-button type="primary" @click="$parent.addFormVisible = false"
-        >确 定</el-button
-      >
+      <el-button type="primary" @click="submitAddForm">确 定</el-button>
     </div>
   </el-dialog>
 </template>
 
 <script>
+// 导入 新增方法
+import { subjectAdd } from "@/api/subject.js";
 export default {
   name: "subject-add",
   data() {
     return {
-      form: {},
+      addForm: {
+        rid:'',
+        name:'',
+        short_name:'',
+        intro:"",
+        remark:""
+      },
+      rules: {
+        rid: [{ required: true, message: "学科编号不能为空", trigger: "blur" }],
+        name: [{ required: true, message: "学科名称不能为空", trigger: "blur" }]
+      },
       formLabelWidth: "80px"
     };
+  },
+  methods: {
+    submitAddForm() {
+      this.$refs.addForm.validate(valid => {
+        if (valid) {
+          subjectAdd(this.addForm).then(res=>{
+            // console.log(res)
+            if(res.data.code===200){
+              // 重新表单
+              this.$refs.addForm.resetFields()
+              this.$parent.addFormVisible = false;
+              // 重新获取数据
+              this.$parent.getList();
+            }
+          })
+        } else {
+          this.$message.warning("学科信息输入有误，请检查");
+          return false;
+        }
+      });
+    }
   }
 };
 </script>
@@ -52,6 +83,5 @@ export default {
   .el-dialog {
     width: 603px;
   }
- 
 }
 </style>
