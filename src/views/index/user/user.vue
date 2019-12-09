09586e0932f41ea45e305c781fb9390731cb20b5
autumnFish <517729329@qq.com>
2019-12-09 17:12:50 +0800

    用户模块 - 页面布局

--- src/views/index/user/user.vue
+++ src/views/index/user/user.vue
@@ -3,25 +3,28 @@
     <!-- 头部 -->
     <el-card class="card-header">
       <el-form :inline="true" :model="filterForm" class="demo-form-inline">
-        <el-form-item label="用户编号">
+        <el-form-item label="用户名称">
           <el-input v-model="filterForm.user" class="short-input"></el-input>
         </el-form-item>
-        <el-form-item label="用户名称">
+        <el-form-item label="用户邮箱">
           <el-input v-model="filterForm.user"></el-input>
         </el-form-item>
-        <el-form-item label="创建者">
-          <el-input v-model="filterForm.user" class="short-input"></el-input>
-        </el-form-item>
-        <el-form-item label="状态">
+        <el-form-item label="角色">
           <el-select v-model="filterForm.region" placeholder="请选择状态">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
+            <el-option label="学生" value="2"></el-option>
+            <el-option label="老师" value="3"></el-option>
+            <el-option label="管理员" value="4"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
           <el-button type="primary">查询</el-button>
           <el-button>清除</el-button>
-          <el-button type="primary" @click="addFormVisible=true" icon="el-icon-plus">新增用户</el-button>
+          <el-button
+            type="primary"
+            @click="addFormVisible = true"
+            icon="el-icon-plus"
+            >新增用户</el-button
+          >
         </el-form-item>
       </el-form>
     </el-card>
@@ -44,15 +47,14 @@
         <el-table-column prop="h" label="操作">
           <template slot-scope="scope">
             <el-button type="text">编辑</el-button>
-            <el-button
-              type="text"
-              >{{scope.row.g===0?"启用":"禁用"}}</el-button
-            >
+            <el-button type="text">{{
+              scope.row.g === 0 ? "启用" : "禁用"
+            }}</el-button>
             <el-button type="text">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
-       <!-- 分页器 -->
+      <!-- 分页器 -->
       <el-pagination
         :current-page="1"
         :page-sizes="[5, 10, 15, 20]"
@@ -64,17 +66,17 @@
       </el-pagination>
     </el-card>
     <!-- 对话框 -->
-    <userDialog/>
+    <userDialog />
   </div>
 </template>
 
 <script>
 // 导入并使用
-import userDialog from './components/userDialog.vue'
+import userDialog from "./components/userDialog.vue";
 export default {
   name: "user",
   // 注册组件
-  components:{
+  components: {
     userDialog
   },
   data() {
@@ -97,7 +99,7 @@ export default {
           f: "部分权限的老师",
           g: 1
         },
-                {
+        {
           b: "ice",
           c: "16666666666",
           d: "6666@qq.com",
@@ -107,7 +109,7 @@ export default {
         }
       ],
       // 是否显示新增框
-      addFormVisible:false
+      addFormVisible: false
     };
   }
 };
