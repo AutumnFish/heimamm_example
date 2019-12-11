431f0cb5bb3c74b47d46bc7572999d88e61caf1d
autumnFish <517729329@qq.com>
2019-12-11 10:49:59 +0800

    题库模块 - 新增试题

--- src/views/index/question/components/questionDialog.vue
+++ src/views/index/question/components/questionDialog.vue
@@ -6,7 +6,7 @@
     fullscreen
     @opened="opened"
   >
-    <el-form :model="form" label-position="left" ref="addForm">
+    <el-form :model="form" label-position="left" ref="addForm" :rules="rules">
       <el-form-item label="学科" prop="subject" :label-width="formLabelWidth">
         <el-select v-model="form.subject" placeholder="请选择学科">
           <el-option
@@ -75,10 +75,10 @@
         label="单选"
         class="single-item"
         :label-width="formLabelWidth"
-        v-if="form.radio === '单选'"
-        prop="singe_select_answer"
+        v-if="form.type === '1'"
+        prop="single_select_answer"
       >
-        <el-radio-group v-model="form.radio">
+        <el-radio-group v-model="form.single_select_answer">
           <div class="radio-box">
             <el-radio label="A">A</el-radio>
             <el-input
@@ -92,11 +92,58 @@
               :on-success="handleASuccess"
               :before-upload="beforeAvatarUpload"
             >
-              <img
-                v-if="form.select_options[0].image"
-                :src="form.select_options[0].image"
-                class="avatar"
-              />
+              <img v-if="imageAUrl" :src="imageAUrl" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </div>
+          <div class="radio-box">
+            <el-radio label="B">B</el-radio>
+            <el-input
+              v-model="form.select_options[1].text"
+              placeholder=""
+            ></el-input>
+            <el-upload
+              class="avatar-uploader"
+              :action="uploadAction"
+              :show-file-list="false"
+              :on-success="handleBSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageBUrl" :src="imageBUrl" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </div>
+          <div class="radio-box">
+            <el-radio label="C">C</el-radio>
+            <el-input
+              v-model="form.select_options[2].text"
+              placeholder=""
+            ></el-input>
+            <el-upload
+              class="avatar-uploader"
+              :action="uploadAction"
+              :show-file-list="false"
+              :on-success="handleCSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageCUrl" :src="imageCUrl" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </div>
+          <div class="radio-box">
+            <el-radio label="D">D</el-radio>
+            <el-input
+              v-model="form.select_options[3].text"
+              placeholder=""
+            ></el-input>
+            <el-upload
+              class="avatar-uploader"
+              :action="uploadAction"
+              :show-file-list="false"
+              :on-success="handleDSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageDUrl" :src="imageDUrl" class="avatar" />
               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
           </div>
@@ -107,10 +154,11 @@
         label="多选"
         class="multiple-item"
         :label-width="formLabelWidth"
-        v-else-if="form.radio === '多选'"
+        v-else-if="form.type === '2'"
         prop="multiple_select_answer"
       >
-        <el-checkbox-group v-model="form.radio">
+        <el-checkbox-group v-model="form.multiple_select_answer">
+          <!-- 选项A -->
           <div class="radio-box">
             <el-checkbox label="A">A</el-checkbox>
             <el-input
@@ -124,11 +172,61 @@
               :on-success="handleASuccess"
               :before-upload="beforeAvatarUpload"
             >
-              <img
-                v-if="form.select_options[0].image"
-                :src="form.select_options[0].image"
-                class="avatar"
-              />
+              <img v-if="imageAUrl" :src="imageAUrl" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </div>
+          <!-- 选项B -->
+          <div class="radio-box">
+            <el-checkbox label="B">B</el-checkbox>
+            <el-input
+              v-model="form.select_options[1].text"
+              placeholder=""
+            ></el-input>
+            <el-upload
+              class="avatar-uploader"
+              :action="uploadAction"
+              :show-file-list="false"
+              :on-success="handleBSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageBUrl" :src="imageBUrl" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </div>
+          <!-- 选项C -->
+          <div class="radio-box">
+            <el-checkbox label="C">C</el-checkbox>
+            <el-input
+              v-model="form.select_options[2].text"
+              placeholder=""
+            ></el-input>
+            <el-upload
+              class="avatar-uploader"
+              :action="uploadAction"
+              :show-file-list="false"
+              :on-success="handleCSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageCUrl" :src="imageCUrl" class="avatar" />
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </div>
+          <!-- 选项D -->
+          <div class="radio-box">
+            <el-checkbox label="D">D</el-checkbox>
+            <el-input
+              v-model="form.select_options[3].text"
+              placeholder=""
+            ></el-input>
+            <el-upload
+              class="avatar-uploader"
+              :action="uploadAction"
+              :show-file-list="false"
+              :on-success="handleDSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageDUrl" :src="imageDUrl" class="avatar" />
               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
           </div>
@@ -137,7 +235,7 @@
       <!-- 简答 -->
       <el-form-item v-else label="简答" class="answer-item" prop="short_answer">
         <el-input
-          v-model="form.answer"
+          v-model="form.short_answer"
           type="textarea"
           rows="3"
           placeholder=""
@@ -146,7 +244,12 @@
       <!-- 时间线 -->
       <el-divider></el-divider>
       <!-- 解析视频 -->
-      <el-form-item label="解析视频" prop="video" :label-width="formLabelWidth">
+      <el-form-item
+        label="解析视频"
+        prop="video"
+        class="video-item"
+        :label-width="formLabelWidth"
+      >
         <el-upload
           class="avatar-uploader"
           :action="uploadAction"
@@ -159,6 +262,7 @@
             只能上传video/mp4文件，且不超过2048kb
           </div>
         </el-upload>
+        <video class="video" controls :src="videoUrl"></video>
       </el-form-item>
       <!-- 时间线 -->
       <el-divider></el-divider>
@@ -195,15 +299,16 @@ export default {
   data() {
     return {
       form: {
-        title: "那种包子最好吃",
+        title: "",
         type: "1",
         subject: 11,
         step: "1",
         enterprise: 15,
-        difficulty: "2",
-        single_select_answer: "A",
-        video: "upload/20191129/bd666ff11c11cc01f494d6ba49757a64.png",
-        remark: "好好吃东西哦",
+        difficulty: 2,
+        single_select_answer: "",
+        multiple_select_answer: [],
+        video: "",
+        remark: "",
         city: ["天津市", "市辖区"],
         short_answer: "狗不理当然是最美味的啦",
         answer_analyze: "",
@@ -230,24 +335,67 @@ export default {
           }
         ]
       },
+      rules: {
+        title: { required: true, message: "标题不能为空" },
+        type: { required: true, message: "类型不能为空" },
+        subject: { required: true, message: "学科不能为空" },
+        step: { required: true, message: "阶段不能为空" },
+        enterprise: { required: true, message: "企业不能为空" },
+        difficulty: { required: true, message: "难度不能为空" },
+        single_select_answer: { required: true, message: "单选题答案不能为空" },
+        multiple_select_answer: {
+          required: true,
+          message: "多选题答案不能为空"
+        },
+        video: { required: true, message: "视频不能为空" },
+        remark: { required: true, message: "备注不能为空" },
+        city: { required: true, message: "城市不能为空" },
+        short_answer: { required: true, message: "简答题答案不能为空" },
+        answer_analyze: { required: true, message: "答案解析不能为空" },
+        select_options: { required: true, message: "选项不能为空" }
+      },
       // 图片的上传地址
       uploadAction: process.env.VUE_APP_BASEURL + "/question/upload",
       formLabelWidth: "80px",
       // 级联选择器数据
       options: provinceAndCityData,
       titleEditor: undefined,
-      answerEditor: undefined
+      answerEditor: undefined,
+      // 图片预览地址
+      imageAUrl: "",
+      imageBUrl: "",
+      imageCUrl: "",
+      imageDUrl: "",
+      // 视频预览地址
+      videoUrl: ""
     };
   },
 
   methods: {
     // 提交数据
     submitForm() {
-      questionAdd(this.form).then(res => {
-        console.log(res);
-        this.$refs.addForm.resetFields()
-        this.$parent.addFormVisible = false;
-        this.$parent.getList()
+      this.$refs.addForm.validate(valid => {
+        if (valid) {
+          questionAdd(this.form).then(res => {
+            if(res.code===200){
+            this.$refs.addForm.resetFields();
+            // 富文本的清空需要自己来
+            this.titleEditor.txt.html('')
+            this.answerEditor.txt.html('')
+            // 预览地址清空
+            this.imageAUrl = ''
+            this.imageBUrl = ''
+            this.imageCUrl = ''
+            this.imageDUrl = ''
+            this.videoUrl = ''
+            this.$parent.addFormVisible = false;
+            this.$parent.getList();
+            }
+          });
+        } else {
+          this.$message.warning("题库信息输入有误，请检查");
+          return false;
+        }
       });
     },
     opened() {
@@ -278,17 +426,33 @@ export default {
         this.answerEditor.create();
       }
     },
-    handleVideoSuccess() {},
+    handleVideoSuccess(res, file) {
+      console.log(res);
+      this.videoUrl = URL.createObjectURL(file.raw);
+      this.form.video = res.data.url;
+    },
     handleASuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
-      this.form.select_options[0].image = res.data.file_path;
+      this.imageAUrl = URL.createObjectURL(file.raw);
+      this.form.select_options[0].image = res.data.url;
+    },
+    handleBSuccess(res, file) {
+      this.imageBUrl = URL.createObjectURL(file.raw);
+      this.form.select_options[1].image = res.data.url;
+    },
+    handleCSuccess(res, file) {
+      this.imageCUrl = URL.createObjectURL(file.raw);
+      this.form.select_options[2].image = res.data.url;
+    },
+    handleDSuccess(res, file) {
+      this.imageDUrl = URL.createObjectURL(file.raw);
+      this.form.select_options[3].image = res.data.url;
     },
     beforeAvatarUpload(file) {
-      const isJPG = file.type === "image/jpeg";
+      const isJPG = file.type === "image/jpeg" || file.type === "image/png";
       const isLt2M = file.size / 1024 / 1024 < 2;
 
       if (!isJPG) {
-        this.$message.error("上传头像图片只能是 JPG 格式!");
+        this.$message.error("上传头像图片只能是 JPG或PNG 格式!");
       }
       if (!isLt2M) {
         this.$message.error("上传头像图片大小不能超过 2MB!");
@@ -296,6 +460,7 @@ export default {
       return isJPG && isLt2M;
     },
     beforeVideoUpload(file) {
+      console.log(file);
       const isJPG = file.type === "video/mp4";
       const isLt2M = file.size / 1024 / 1024 < 2;
 
@@ -377,6 +542,9 @@ export default {
     width: 178px;
     height: 178px;
     display: block;
+  }
+  .video {
+    width: 360px;
   }
 }
 </style>
