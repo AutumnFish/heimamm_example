b15ea55499e1a7743ab1d9c5a3ba5448591e0205
autumnFish <517729329@qq.com>
2020-02-06 21:16:44 +0800

    移除subject.vue中的逻辑代码

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -27,8 +27,8 @@
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="filterData">查询</el-button>
-          <el-button @click="resetFilter">清除</el-button>
+          <el-button type="primary" >查询</el-button>
+          <el-button >清除</el-button>
           <el-button
             type="primary"
             @click="addFormVisible = true"
@@ -60,12 +60,12 @@
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button type="text" @click="enterEdit(scope.row)">编辑</el-button>
-            <el-button type="text" @click="changeState(scope.row)">{{
+            <el-button type="text" @click="enterEdit">编辑</el-button>
+            <el-button type="text" >{{
               scope.row.status === 0 ? "启用" : "禁用"
             }}</el-button>
-            <!-- <el-button type="text" v-if="['超级管理员'].includes($store.state.userInfo.role)" @click="removeSubject(scope.row)"> -->
-            <el-button type="text" v-power="['超级管理员']" @click="removeSubject(scope.row)">
+           
+            <el-button type="text" >
               删除
             </el-button>
           </template>
@@ -79,8 +79,6 @@
         layout="total, sizes, prev, pager, next, jumper"
         :total="total"
         background
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
       >
       </el-pagination>
     </el-card>
@@ -96,7 +94,7 @@
 import subjectDialog from "./components/subjectDialog.vue";
 import subjectEditDialog from "./components/subjectEditDialog.vue";
 // 导入学科接口
-import { subjectList, subjectStatus, subjectRemove } from "@/api/subject.js";
+import { subjectList } from "@/api/subject.js";
 export default {
   name: "subject",
   // 注册组件
@@ -112,22 +110,7 @@ export default {
         status: ""
       },
       subjectTable: [
-        {
-          b: "subject1",
-          c: "前端与移动开发",
-          d: "前端",
-          e: "管理员",
-          f: "2019-12-4",
-          g: 0
-        },
-        {
-          b: "subject2",
-          c: "Java",
-          d: "后端",
-          e: "管理员",
-          f: "2019-12-5",
-          g: 1
-        }
+       
       ],
       // 是否显示新增框
       addFormVisible: false,
@@ -145,60 +128,11 @@ export default {
   },
   methods: {
     // 进入编辑状态
-    enterEdit(item){
-      // 数据传递
-      this.$refs.subjectEditDialog.editForm = JSON.parse(JSON.stringify(item));
+    enterEdit(){
       // 显示编辑框
       this.editFormVisible = true;
     },
-    // 删除数据
-    removeSubject(item) {
-      this.$confirm("此操作将删除这个学科, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
-      })
-        .then(() => {
-          subjectRemove({
-            id: item.id
-          }).then(res => {
-            if (res.data.code !== 200) {
-              return this.$message.error(res.data.message);
-            }
-            // 重新获取数据
-            this.getList();
-          });
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "已取消删除"
-          });
-        });
-    },
-    filterData() {
-      // 去第一页
-      this.page = 1;
-      // 重新获取数据
-      this.getList();
-    },
-    resetFilter() {
-      this.$refs.filterForm.resetFields();
-    },
-    // 修改状态
-    changeState(item) {
-      subjectStatus({
-        id: item.id
-      }).then(res => {
-        // console.log(res)
-        if (res.code === 200) {
-          // 提示用户
-          this.$message.success("状态修改成功");
-          // 重新获取数据
-          this.getList();
-        }
-      });
-    },
+  
     // 获取数据
     getList() {
       // 初始数据获取不携带任何数据
@@ -211,21 +145,10 @@ export default {
         this.subjectTable = res.data.items;
         // 总条数
         this.total = res.data.pagination.total;
+        window.console.log(JSON.stringify(this.subjectTable))
       });
     },
-    // 页容量改变
-    handleSizeChange(limit) {
-      this.limit = limit;
-      this.page = 1;
-      // 重新获取数据
-      this.getList();
-    },
-    // 页码改变
-    handleCurrentChange(page) {
-      this.page = page;
-      // 重新获取数据
-      this.getList();
-    }
+    
   },
   // 获取列表数据
   created() {
